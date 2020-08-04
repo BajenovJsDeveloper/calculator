80791544759affa15535f5b72a7be89f32dbcc22
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-08-05 00:39:34 +0300

    Create logic ...

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,44 +2,43 @@ import React, { useState } from 'react';
 import '../index.scss';
 
 interface ResultProps {
-  result: string;
+  result: string | null;
 }
 
 interface EqualProps {
-  hdlEqual: () => void;
+  handleEqual: () => void;
 }
 
 interface DigitsProps {
-  hdlClick: (num: string | undefined) => void;
+  handleDigit: (num: string | undefined) => void;
 }
 
 const EqualButton: React.FC<EqualProps> = (
-  props: EqualProps
-): React.ReactElement => {
-  const { hdlEqual } = props;
-  const clickEqual = () => {
-    hdlEqual();
-  };
+    props: EqualProps
+  ): React.ReactElement => {
+  const { handleEqual } = props;
+  
   return (
-    <div className="box equal" onClick={clickEqual}>
+    <div className="box equal" onClick={handleEqual}>
       =
     </div>
   );
 };
 
 const DisplayResult: React.FC<ResultProps> = (
-  props: ResultProps
-): React.ReactElement => {
-  const { result } = props;
+    props: ResultProps
+  ): React.ReactElement => {
+  let { result } = props;
+  result = result === null? '0': result; 
   return <div className="box result">{result}</div>;
 };
 
-const Digits: React.FC<DigitsProps> = (props) => {
-  const { hdlClick } = props;
+const Digits: React.FC<DigitsProps> = (props: DigitsProps) => {
+  const { handleDigit } = props;
   const digits = [0, 7, 8, 9, 4, 5, 6, 1, 2, 3];
   const numberClick = (ev: React.SyntheticEvent<HTMLDivElement>) => {
     const tr = ev.target as HTMLElement;
-    hdlClick(tr.dataset.id);
+    handleDigit(tr.dataset.id);
   };
   return (
     <>
@@ -57,36 +56,108 @@ const Digits: React.FC<DigitsProps> = (props) => {
   );
 };
 
-const App: React.FC = () => {
-  const [result, setResult] = useState<string>('0');
-  const digitsClick = (num: string | undefined) => {
-    if (num) setResult(result + num);
+interface ClearProps {
+  handleClear: () => void;
+}
+
+const ClearButton:React.FC<ClearProps> = (props:ClearProps) => {
+  const { handleClear } = props;
+  return (
+      <div className='box action clear' onClick={handleClear}>clear</div>
+    );
+}
+
+interface ActionsProps{
+  handleAction: (act:string) => void;
+}
+
+const Actions:React.FC<ActionsProps> = (props:ActionsProps) => {
+
+  const { handleAction } = props;
+  const act:Array<string> = ['+','-','*',':'];
+
+  const actClick = (e:React.SyntheticEvent<HTMLDivElement>) => {
+    const item = e.target as HTMLElement;
+    if(!!item.dataset.act) handleAction(item.dataset.act);
+  }
+  return (
+      <div className="actions">
+        {
+          act.map(act => (<div key={act} 
+            className="box action" 
+            onClick={actClick}
+            data-act={act}>{act}</div>))
+        }
+      </div>
+    );
+}
+
+const resultInit:ResultInit  = {
+  res: null,
+  a: null,
+  b:null,
+  action:null,
+}
+interface ResultInit {
+  res: null | string;
+  a: null | string;
+  b: null | string;
+  action: null | string;
+}
+
+const Calculator: React.FC = () => {
+  const [result, setResult] = useState<ResultInit>(resultInit);
+  // const [nums, setNums] = useState<Array<number>>([0,0]);
+
+  const handleDigit = (num: string | undefined) => {
+    if(num){
+      const res = (result.res === null) ? '' : result.res;
+      if(!result.action) {
+        const a = (result.a === null) ? '' : result.a;
+        setResult({...result, a: `${a}${num}`, res: `${res}${num}`});
+      }
+      else{
+        const b = (result.b === null) ? '' : result.b;
+        setResult({...result, b: `${b}${num}`, res: `${res}${num}`});
+      }
+    }
   };
 
-  const hdlClickEqual = () => {
-    setResult('nothing');
+  const handleEqual = () => {
+    // setResult('nothing');
   };
+
+  const handleClear = () => {
+    setResult(resultInit);
+  };
+  const handleAction = (act:string) => {
+    const res = (result.res === null)? '' : result.res;
+    if(result.a && !result.action){
+       setResult({...result, action: act, res: `${res} ${act} `});
+    }
+    //------------------------  fix this problem   
+    else {
+      
+      setResult({...result, action: act, res: `${res.slice(0,-2)} ${act} `});
+    }
+    //--------------------------------------------
+  }
 
   return (
     <>
       <div className="container">
-        <DisplayResult result={result} />
+        <DisplayResult result={result.res} />
         <div className="digits">
           <div className="d-grid">
-            <div className="box">clr</div>
-            <EqualButton hdlEqual={hdlClickEqual} />
-            <Digits hdlClick={digitsClick} />
+            <ClearButton handleClear={handleClear}/>
+            <EqualButton handleEqual={handleEqual} />
+            <Digits handleDigit={handleDigit} />
           </div>
         </div>
-        <div className="actions">
-          <div className="box action">*</div>
-          <div className="box action">+</div>
-          <div className="box action">-</div>
-          <div className="box action">:</div>
-        </div>
+        <Actions handleAction={handleAction}/>
       </div>
     </>
   );
 };
 
-export default App;
+export default Calculator;
